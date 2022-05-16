c01b62a2517ab2f1974f14474570005f2aea5725
Natacha Vergara <ntchvergara@gmail.com>
2022-05-16 12:28:07 -0300

    path public folder

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 // view engine setup
 app.set("views", path.join(__dirname, "./src/views"));
 app.set("view engine", "ejs");
-
+app.use(express.static(path.join(__dirname, "public")));
 
 
 app.use('/users', usersRoutes)
