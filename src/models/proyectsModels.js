32fd7206a8aa30d6c00713920f9c8ead836e71ba
Natacha Vergara <ntchvergara@gmail.com>
2022-05-18 11:15:08 -0300

    modified proyects models to data: [...data]

--- src/models/proyectsModels.js
+++ src/models/proyectsModels.js
@@ -27,7 +27,7 @@ const createProyect = async ({ title, link, logo, img, tec }) => {
     return {
         id: data.insertId,
         proyect: { title, link, logo, img, tec },
-        data: [...data],
+        data,
         message: `New proyect created successfully`
     }
 }
@@ -56,9 +56,7 @@ const deleteProyect = async (id) => {
     `)
 
     return {
-
         id,
-        data: [...data],
         deleted: data.affectedRows ? true : false,
         message: "The proyect has been deleted" 
     }
