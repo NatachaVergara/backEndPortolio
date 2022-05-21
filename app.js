26eeba0a50d781411994469b7484160909b857dd
Natacha Vergara <ntchvergara@gmail.com>
2022-05-21 12:47:55 -0300

    solucionando problemas de cors

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const proyectsRouter = require('./src/routes/proyectsRoutes')
 
 /***************** */
 const corsOptions = {
-  origin:'http://localhost:3000', 
+  origin:'http://localhost:3002', 
   credentials: true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
