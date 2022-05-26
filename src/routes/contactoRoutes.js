8593aa13d122c840c487d4582510c22546d5514f
Natacha Vergara <ntchvergara@gmail.com>
2022-05-26 19:31:11 -0300

    probando la ruta de contacto

--- src/routes/contactoRoutes.js
+++ src/routes/contactoRoutes.js
@@ -7,11 +7,9 @@ router.get('/')
 router.post('/', async (req, res) => {
     const { name, email, razon, msg } = req.body
     console.log(name, email, razon, msg)
-
-
     try {
         const sended = await (sendMail({ to: email, subject: `${name} - ${email}`, text: msg }))
-        return res.send({ sended: true })
+        return res.send({ sended })
 
     } catch (error) {
         console.log(error)
