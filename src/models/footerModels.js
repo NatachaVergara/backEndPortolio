dce2a24f512e7f62affce6f80be53cd319bf8b41
Natacha Vergara <ntchvergara@gmail.com>
2022-05-26 16:35:09 -0300

    arreglando algunos pequeños errores en la ruta footer

--- src/models/footerModels.js
+++ src/models/footerModels.js
@@ -46,7 +46,7 @@ const updateLinkModel = async (id, link, name, img) => {
         `UPDATE footerLinks SET 
         link = "${link}",
         name = "${name}",
-        img = "${img}"
+        img = "<${img}/>"
         WHERE id = ${id}
         `)
 
