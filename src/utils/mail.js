f31d2fabf415eed2df377d0df199a5cc05f9541f
Natacha Vergara <ntchvergara@gmail.com>
2022-05-27 14:39:03 -0300

    intentando hacer funcionar al nodemailer

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -8,11 +8,11 @@ module.exports.sendMail = ({to, subject, text}) => new Promise((resolve, reject)
         service: 'gmail',
         auth: {
             type: 'OAuth2',
-            user: "ntchvergara@gmail.com",
-            pass: "Elisabet1986",
-            clienteId: '383376051581-9ja3sovb4l5cti1dbv30paog2icv9m4o.apps.googleusercontent.com',
-            clientSecret: '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',
-            refreshToken: '1//04HRr29E1fwSKCgYIARAAGAQSNwF-L9IrTdY19pyUS5NziqyA2qxGarebb8LtjQlVIZZu1jFYlKOwE9KPDqY6ex9_pXYSyfY4L_E'
+            user: MAIL_USERNAME,
+            pass: MAIL_PASSWORD,
+            clienteId: OAUTH2_CLIENTID,
+            clientSecret: OAUTH2_CLIENT_SECRET,
+            refreshToken: OAUTH2_REFRESH_TOKEN
         }
     });
 
