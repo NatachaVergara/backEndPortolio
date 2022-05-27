504450c517deb0d0174a6d7770837c7150685609
Natacha Vergara <ntchvergara@gmail.com>
2022-05-27 16:46:59 -0300

    descargue e instale el dotenv

--- src/config/mail_credentials.config.js
+++ src/config/mail_credentials.config.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 module.exports = {
-    MAIL_USERNAME: 'ntchvergara@gmail.com',
-    MAIL_PASSWORD: 'Elisabet1986',
-    OAUTH2_CLIENTID: '383376051581-ul16o7gdtdo1d0k4boso72ervae3169j.apps.googleusercontent.com',
-    OAUTH2_CLIENT_SECRET: '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',
-    OAUTH2_REFRESH_TOKEN: '1//04KIv0vtJ5FD9CgYIARAAGAQSNwF-L9IrK4N8DT7YD_1fssR_R6e-hUkuxLzHEdaLJe0CIPpIwKeocaO3d5QzF1NsfQbPjaQ3Wv0'
+    MAIL_USERNAME: process.env.NM_USERNAME,
+    MAIL_PASSWORD: process.env.NM_PASSWORD,
+    OAUTH2_CLIENTID: process.env.NM_OAUTH2_CLIENTID,
+    OAUTH2_CLIENT_SECRET: process.env.NM_OAUTH2_SECRET_CLIENT ,
+    OAUTH2_REFRESH_TOKEN: process.env.NM_OAUTH2_REFRESH_TOKEN
 }
