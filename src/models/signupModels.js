35eef1b4fe003f81ad910acf9a9f34dece3b863e
Natacha Vergara <ntchvergara@gmail.com>
2022-05-30 21:53:43 -0300

    me esta dando error de servidor

--- src/models/signupModels.js
+++ src/models/signupModels.js
@@ -3,16 +3,24 @@ const { request } = require('../db/request')
 const { hashPassword } = require('../utils/password')
 
 const signUp = async (user, password) => {
-    const hasedPassword = hashPassword(password)
-    const data = await request(
-        `INSERT INTO users(email, password, type)
-            VALUES("${user}", "${hasedPassword}", "ADMIN")
-        `)
+    const existe = await request(`SELECT * FROM users WHERE email = '${user}'`)
 
-    return {
-        id: data.insertId,
-        user
+    if (existe.length === 0) {
+        return 'Ese usuario ya existe'
+    } else {
+        const hasedPassword = hashPassword(password)
+        const data = await request(
+            `INSERT INTO users(email, password, type)
+                VALUES("${user}", "${hasedPassword}", "ADMIN")
+            `)
+
+        return {
+            id: data.insertId,
+            user
+        }
     }
+
+
 }
 
 
