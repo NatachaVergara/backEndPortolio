aa551a1f6d4bef6e80e94f3bcda11999fa05a7e9
Natacha Vergara <ntchvergara@gmail.com>
2022-06-01 11:57:41 -0300

    actualizando el modelo y controler de signup y creando un usuario de prueba

--- src/models/signupModels.js
+++ src/models/signupModels.js
@@ -5,7 +5,7 @@ const { hashPassword } = require('../utils/password')
 const signUp = async (email, password, type) => {
     const existe = await request(`SELECT * FROM users WHERE email = '${email}'`)
 
-    if (existe.length === 0) {
+    if (existe.length !== 0) {
         return 'Ese usuario ya existe'
     } else {
         const hasedPassword = hashPassword(password)
