f921b003118531a24897c277168ff8d72ef1081e
Natacha Vergara <ntchvergara@gmail.com>
2022-06-07 15:16:18 -0300

    multer no funciona, viendo porque

--- src/middlewares/imagesUpload.js
+++ src/middlewares/imagesUpload.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
     destination: path.join(__dirname, '../public/images'),
     filename:  (req, file, cb) => {
         console.log("storage fileName ", file)
-        cb(null, `${Date.now()}${path.extname(file.name)}`);
+        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
     }
 })
 
