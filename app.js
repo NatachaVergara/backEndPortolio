d127dc91d4558d1c29a3b17ff4e007c5e498ec45
Natacha Vergara <ntchvergara@gmail.com>
2022-06-08 12:21:52 -0300

    multer no funciona, viendo porque

--- app.js
+++ app.js
@@ -47,10 +47,10 @@ app.use('/images', imageRouter)
 
 
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 
 // // error handler
