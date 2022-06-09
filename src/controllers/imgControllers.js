69e4b7e8581fa8db19f440646a840502c75a9cdb
Natacha Vergara <ntchvergara@gmail.com>
2022-06-09 20:14:59 -0300

    aaaaa

--- src/controllers/imgControllers.js
+++ src/controllers/imgControllers.js
@@ -6,7 +6,7 @@ const uploadImgs = require('../models/imgModel')
 const storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
-        cb(null, './img')
+        cb(null, '/img')
       },   
 
 
