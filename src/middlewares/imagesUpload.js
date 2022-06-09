6249fa2ae02fdb8908c02805f9a557a22a7d8386
Natacha Vergara <ntchvergara@gmail.com>
2022-06-09 10:57:19 -0300

    siguen el problema

--- src/middlewares/imagesUpload.js
+++ src/middlewares/imagesUpload.js
@@ -5,11 +5,11 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname + '/img')  )
-      },
+        cb(null, 'img')
+    },
 
-    filename: (req, file, cb) => {        
-        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
 
         console.log("storage fileName: ", file)
     }
