11bfdb8cc7282dc53cee81f168fb226d93ae6e7e
Natacha Vergara <ntchvergara@gmail.com>
2022-06-10 16:03:56 -0300

    sigo con multer

--- src/models/imgModel.js
+++ src/models/imgModel.js
@@ -11,7 +11,7 @@ const getImgs = async () => {
     }
 }
 
-const createImg = async (file) => {
+const createImg = async ({file}) => {
     console.log(`Modelo: ${file}`)
 //     const img = await request(`
 //         INSERT INTO skill_img(path , upload_date)
