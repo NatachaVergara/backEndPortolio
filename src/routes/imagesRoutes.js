2c37973d88ff29f69a48872392be7f924893e857
Natacha Vergara <ntchvergara@gmail.com>
2022-06-10 15:38:41 -0300

    sigo con multer

--- src/routes/imagesRoutes.js
+++ src/routes/imagesRoutes.js
@@ -10,7 +10,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname,'../public/images/'))
+      cb(null, path.join(__dirname,'../../public/images/'))
     },
     filename: function (req, file, cb) {
      
