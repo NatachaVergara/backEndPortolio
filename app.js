e872d95ed50584a560fc6d5e42925fe6ff948856
Natacha Vergara <ntchvergara@gmail.com>
2022-06-10 14:39:44 -0300

    sigo con multer

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
