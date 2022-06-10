124fa6f777f555adefb16cce4778269b15db0257
Natacha Vergara <ntchvergara@gmail.com>
2022-06-10 15:58:11 -0300

    sigo con multer

--- src/routes/imagesRoutes.js
+++ src/routes/imagesRoutes.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const router = express.Router()
 const controller = require('../controllers/imgControllers')
-
 const path = require("path");
 // const fs = require('fs');
 const multer = require('multer');
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        console.log("Destination: ", file)
-      cb(null, 'public/images/')
-    },
+    // destination: function (req, file, cb) {
+    //     console.log("Destination: ", file)
+    //   cb(null, 'public/images/')
+    // },
     filename: function (req, file, cb) {
-     
+        console.log("File name: ", file)
       cb(null, `${Date.now()}${path.extname(file.originalname)}` )
     }
-  })
-  
+  });  
   const upload = multer({ storage: storage })
-
+  router.post('/', upload.single('image'), controller.createImgController )
 
 
 
@@ -27,7 +24,7 @@ router.get('/', controller.imgsController)
 
 
 
-router.post('/', upload.single('image'), controller.createImgController )
+
 
 
 module.exports = router
