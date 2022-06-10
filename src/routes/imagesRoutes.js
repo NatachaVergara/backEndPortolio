807cd5c50c5e1a410d89139a9514914eb2df3205
Natacha Vergara <ntchvergara@gmail.com>
2022-06-10 15:47:31 -0300

    sigo con multer

--- src/routes/imagesRoutes.js
+++ src/routes/imagesRoutes.js
@@ -3,15 +3,13 @@ const router = express.Router()
 const controller = require('../controllers/imgControllers')
 
 const path = require("path");
-const fs = require('fs');
+// const fs = require('fs');
 const multer = require('multer');
-// const upload = multer({ dest: 'public/images/' })
-// fs.renameSync(req.file.path, req.file.path + '.' + req.file.mimetype.split('/')[1]);
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         console.log("Destination: ", file)
-      cb(null, 'images')
+      cb(null, 'public/images/')
     },
     filename: function (req, file, cb) {
      
