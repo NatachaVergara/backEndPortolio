58123553f29aaf54b8d0cee51f4ba3890b2ac837
Natacha Vergara <ntchvergara@gmail.com>
2022-06-10 16:55:45 -0300

    arregle el la view del back

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const logger = require("morgan");
-//const createError = require("http-errors");
+const createError = require("http-errors");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
@@ -28,6 +28,9 @@ const corsOptions = {
 
 
 //middleware
+// view engine setup
+app.set("views", path.join(__dirname, "./src/views"));
+app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -36,9 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// // view engine setup
-// app.set("views", path.join(__dirname, "./src/views"));
-// app.set("view engine", "ejs");
+
 // app.use(express.static(path.join(__dirname, "public")));
 
 
@@ -53,22 +54,22 @@ app.use('/images', imageRouter)
 
 
 
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 
 app.listen(PORT, () => { console.log(`Server active at port: ${PORT}`) })
