afd8bff79c420d7876c704d2050f4c217e756a9c
Natacha Vergara <ntchvergara@gmail.com>
2022-06-12 11:26:22 -0300

    sigue el problema con multer

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //Routes
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/signin', signinRoutes)
 app.use('/signup', signupRoutes)
 app.use('/proyects', proyectsRouter)
