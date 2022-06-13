686ff219310e65f684f0d613b9229c8af37b7c85
Natacha Vergara <ntchvergara@gmail.com>
2022-06-13 18:13:54 -0300

    probando multer

--- src/routes/imagesRoutes.js
+++ src/routes/imagesRoutes.js
@@ -4,20 +4,22 @@ const controller = require('../controllers/imgControllers')
 
 
 const path = require("path");
-// const fs = require('fs');
+const fs = require('fs');
 
 const multer = require('multer');
 // upload = multer({ dest: "./public/images" });
 
 const storage = multer.diskStorage({
-  destination: './public/files',
+  //destination: './public/files',
   
-  // destination: function (req, file, cb) {
-  //   console.log("Destination: ", file)
-  //   cb(null, 'C:/Users/todom/OneDrive/Escritorio/portfolioFullstack/portfolio_back/public/images')
-  //   cb(null, 'public/images')
-  //   cb(null, path.join(__dirname+'public/images'))
-  // },
+  destination: function (req, file, cb) {
+    console.log("Destination: ", file)
+   // cb(null, 'C:/Users/todom/OneDrive/Escritorio/portfolioFullstack/portfolio_back/public/images')
+   let imagesSkills = './public/files'
+   fs.mkdirSync(imagesSkills,{recursive: true})
+    cb(null, imagesSkills)
+   // cb(null, path.join(__dirname+'public/images'))
+  },
   filename: function (req, file, cb) {  
     console.log("FileName: ", file)
     cb(null, `${Date.now()}-${file.originalname}`)
