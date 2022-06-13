8b040e9f150a0ea7de531bb5b5d11c3d3885bbeb
Natacha Vergara <ntchvergara@gmail.com>
2022-06-13 15:07:05 -0300

    probando multer

--- src/routes/imagesRoutes.js
+++ src/routes/imagesRoutes.js
@@ -7,22 +7,23 @@ const path = require("path");
 // const fs = require('fs');
 
 const multer = require('multer');
+upload = multer({ dest: "../../public/images" });
 
-const storage = multer.diskStorage({
-  // destination: function (req, file, cb) {
-  //   console.log("Destination: ", file)
-  //   cb(null, 'C:/Users/todom/OneDrive/Escritorio/portfolioFullstack/portfolio_back/public/images')
-  //   cb(null, 'public/images')
-  //   cb(null, path.join(__dirname+'public/images'))
-  // },
-  filename: function (req, file, cb) {  
-    console.log("FileName: ", file)
-    cb(null, `${Date.now()}-${file.originalname}`)
-      // cb(null, `${Date.now()}${path.extname(file.originalname)}`)
-  }
-})
+// const storage = multer.diskStorage({
+//   // destination: function (req, file, cb) {
+//   //   console.log("Destination: ", file)
+//   //   cb(null, 'C:/Users/todom/OneDrive/Escritorio/portfolioFullstack/portfolio_back/public/images')
+//   //   cb(null, 'public/images')
+//   //   cb(null, path.join(__dirname+'public/images'))
+//   // },
+//   filename: function (req, file, cb) {  
+//     console.log("FileName: ", file)
+//     cb(null, `${Date.now()}-${file.originalname}`)
+//       // cb(null, `${Date.now()}${path.extname(file.originalname)}`)
+//   }
+// })
 
-const upload = multer({ storage: storage })
+// const upload = multer({ storage: storage })
 
 router.get('/', controller.imgsController)
 router.post('/', upload.single('image'), controller.createImgController)
