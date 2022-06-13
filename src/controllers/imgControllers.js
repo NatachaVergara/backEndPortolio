39b82be7d7e764544745b06e76ad938c7dee90a7
Natacha Vergara <ntchvergara@gmail.com>
2022-06-13 15:21:54 -0300

    probando multer

--- src/controllers/imgControllers.js
+++ src/controllers/imgControllers.js
@@ -19,11 +19,11 @@ const imgsController = async (req, res) => {
 
 const createImgController =  async  (req, res) => {
     console.log("Controller: ", req.file)   
-    // const file = `${req.file.path}-${req.file.originalname}`
-    // console.log(file)
+    const file = req.file.path
+  
     try {
-        console.log("Controller try: ", req.file) 
-        const img = await uploadImgs.createImg( req.file)
+        console.log("Controller try: ",file) 
+        const img = await uploadImgs.createImg( file)
 
         if (img) {
             return res.status(201).send(img)
