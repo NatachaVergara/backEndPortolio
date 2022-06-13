c1c74be9bcd99fea87538238b19489529ffd7a3a
Natacha Vergara <ntchvergara@gmail.com>
2022-06-13 12:25:40 -0300

    probando multer

--- src/routes/imagesRoutes.js
+++ src/routes/imagesRoutes.js
@@ -13,7 +13,8 @@ const storage = multer.diskStorage({
   //   cb(null, 'C:/Users/todom/OneDrive/Escritorio/portfolioFullstack/portfolio_back/public/images')
   // },
   filename: function (req, file, cb) {  
-    cb(null, `${Date.now()}${path.extname(file.originalname)}`)
+    cb(null, `${Date.now()}-${file.originalname}`)
+      // cb(null, `${Date.now()}${path.extname(file.originalname)}`)
   }
 })
 
