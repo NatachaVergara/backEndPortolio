f82206dedbf1904fffd2c03f99e8886209fd54bf
Natacha Vergara <ntchvergara@gmail.com>
2022-06-17 11:23:49 -0300

    creando el middleware de multer

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -1,6 +1,5 @@
+
 const multer = require('multer');
-
-
 const upload = multer({ dest: 'skilImg/' })
 
-module.exports = upload.single('image')
+module.exports = upload
--- src/routes/imagesRoutes.js
+++ src/routes/imagesRoutes.js
@@ -6,11 +6,11 @@ const controller = require('../controllers/imgControllers')
 // const multer = require('multer');
 // const upload = multer({ dest: 'skilImg/' });
 
-const {upload} = require('../middleware/multer')
+const upload = require('../middleware/multer')
 
 router.get('/images', controller.imgsController)
 router.get('/images/:path', controller.imgControler)
-router.post('/', upload, controller.createImgController)
+router.post('/', upload.single('image'), controller.createImgController)
 router.delete('/:path', controller.deleteImgController)
 
 module.exports = router
