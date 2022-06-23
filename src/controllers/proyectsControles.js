21998eac1290b7e9195403642b0d2cb29a42e639
Natacha Vergara <ntchvergara@gmail.com>
2022-06-23 14:56:17 -0300

    vuelvo a intentar hacer funcionar correctamente el update  del edit proyects

--- src/controllers/proyectsControles.js
+++ src/controllers/proyectsControles.js
@@ -64,7 +64,7 @@ const updateProyectController = async (req, res) => {
     let img = ''
     
     //Si del front viene un archivo que elimine la imagen existente en mi bucket y que me cree una nueva y se guearde en la variable img
-    if (!req.file !== undefined) {
+    if (req.file !== undefined) {
        await uploadImgs.deleteImg(image)
         let file = req.file
         img = await uploadImgs.uploadFile(file)
