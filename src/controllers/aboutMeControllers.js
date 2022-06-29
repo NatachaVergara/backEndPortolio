a4f4e7ffa00cbd69af1ec5ec507376c2173fa878
Natacha Vergara <ntchvergara@gmail.com>
2022-06-29 20:18:53 -0300

    creando la ruta/controller/modelo de aboutMe

---
+++ src/controllers/aboutMeControllers.js
@@ -0,0 +1,17 @@
+
+const modelo = require('../models/aboutMeModels')
+
+//Obtengo desde mi db la informacion aboutMe
+export const getAboutMeController = async (req, res) => {}
+
+//Obtengo la imagen directamente desde aws
+export const getAboutMeImgController = async (req, res) => {}
+
+//La tabla aboutMe va a tener 1 solo registro
+export const createAboutMeController = async (req, res) => {}
+
+//Actualizo los datos de mi registro aboutMe
+export const updateAboutMeController = async (req, res) => {}
+
+// //Elimino mi resgistro (no va a estar activo)
+// export const deleteAboutMeController = async (req, res) => {}
---
+++ src/models/aboutMeModels.js
@@ -0,0 +1,14 @@
+const { request } = require('../db/request')
+
+let resgistro
+let registros
+
+
+//Obtengo el registro
+export const getAboutMe = async () => { }
+//Creo el registro
+export const createAboutMe = async () => { }
+//Actualizo el registro 
+export const updateAboutMe = async () => { }
+// //Elimino el registro
+// export const deleteAboutMe = async () => {}
