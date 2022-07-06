91b3ee34b027fc9f004adf53c0ad4a7167688a75
Natacha Vergara <ntchvergara@gmail.com>
2022-07-06 17:20:04 -0300

    arreglando nodemailer que no me aparecia el mail de usuario prueba6

--- src/routes/contactoRoutes.js
+++ src/routes/contactoRoutes.js
@@ -8,7 +8,7 @@ router.post('/', async (req, res) => {
     const { name, email, razon, msg } = req.body
     console.log(name, email, razon, msg)
     try {
-        const sended = await sendMail({ to: email, subject: `${name} - ${razon} - ${email}`, text: msg })
+        const sended = await sendMail({ from: email, subject: `${name} - ${razon} - ${email}`, text: msg })
         return res.status(200).send({ sended })
 
     } catch (error) {
